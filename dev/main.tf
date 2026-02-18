5369e07aedf38ed8daac37ff520e1755e70e287b
ManjushaDevops <manjushaadaling24@gmail.com>
2026-02-18 20:56:47 +0530

    third commit

--- dev/main.tf
+++ dev/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_role_assignment" "rolespn" {
 # }
 
 module "keyvault" {
-  source = "../modules/Keyvault"
+  source = "../modules/keyvault"
   keyvault_name = var.keyvault_name
   location = var.location
   resource_group_name = var.rgname
